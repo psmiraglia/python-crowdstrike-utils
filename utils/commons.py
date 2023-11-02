75ae97d535a21965f6233367bb48769a889442ae
Paolo Smiraglia <paolo.smiraglia@gmail.com>
2023-11-02 12:03:44 +0100

    Add Ready2Move (r2m) script
    The script tags hosts (linked in a specific time range) that can be
    "safely" moved from a group to another. A host is safe if there were
    no detections in the specified time range.
    
    The tag is randomly generated and is in the form of
    
       FalconGroupingTags/r2m-********
    
    If no source group is specified (-g) the query is made on all the linked
    hosts.
    
    If no time range is specified (--from, --to, --days), the script queries
    only on the hosts that were linked in the last 7 days.

---
+++ utils/commons.py
@@ -0,0 +1,115 @@
+'''
+Copyright 2023 Paolo Smiraglia <paolo.smiraglia@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+#
+# Helpers
+#
+
+
+def _api_call(api, command, *args, **kwargs):
+    body = kwargs.get('body', {})
+    offset = kwargs.get('offset', 0)
+    limit = kwargs.get('limit', 5000)
+    sort = kwargs.get('sort', None)
+    filter = kwargs.get('filter', None)
+
+    if body:
+        r = api.command(command, body=body)
+    else:
+        r = api.command(command, offset=offset, limit=limit, sort=sort,
+                        filter=filter)
+    sc = r.get('status_code')
+    if sc < 200 or sc > 299:
+        err = [f'{e["message"]} ({e["code"]})' for e in r['body']['errors']]
+        raise Exception('\n'.join(err))
+    else:
+        return r
+
+
+def _details(api, ids, cb):
+    details = []
+    offset = 100
+    i = 0
+    j = offset
+
+    total = len(ids)
+    while j < total:
+        details.extend(cb(api, ids[i:j]))
+        i = j
+        j += offset
+    details.extend(cb(api, ids[i:total]))
+
+    return details
+
+
+def _query(api, command, filter=None, sort=None):
+    limit = 100
+    total = 1
+    offset = 0
+
+    items = []
+    while offset < total:
+        resp = _api_call(api, command,
+                         filter=filter,
+                         limit=limit,
+                         offset=offset,
+                         sort=sort)
+        items.extend(resp['body']['resources'])
+        total = resp['body']['meta']['pagination']['total']
+        offset = resp['body']['meta']['pagination']['offset']
+        if offset == 0:
+            offset = total
+    return items
+
+#
+# Methods
+#
+
+
+def get_device_details(api, device_ids):
+    def cb(api, device_ids):
+        body = {'ids': device_ids}
+        resp = _api_call(api, 'GetDeviceDetails', body=body)
+        return resp['body']['resources']
+    return _details(api, device_ids, cb)
+
+
+def get_detect_summaries(api, detection_ids):
+    def cb(api, detection_ids):
+        body = {'ids': detection_ids}
+        resp = _api_call(api, 'GetDetectSummaries', body=body)
+        return resp['body']['resources']
+    return _details(api, detection_ids, cb)
+
+
+def query_detects(api, filter=None, sort=None):
+    return _query(api, 'QueryDetects', filter, sort)
+
+
+def query_devices_by_filter(api, filter=None, sort=None):
+    return _query(api, 'QueryDevicesByFilter', filter, sort)
+
+
+def update_device_tags(api, device_ids, action, tags):
+    body = {'action': action, 'device_ids': device_ids, 'tags': tags}
+    resp = _api_call(api, 'UpdateDeviceTags', body=body)
+    return resp
---
+++ utils/r2m.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+'''
+Copyright 2023 Paolo Smiraglia <paolo.smiraglia@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+import datetime
+import logging
+import random
+import string
+from argparse import ArgumentParser
+
+from falconpy import APIHarnessV2
+from tabulate import tabulate
+
+import commons
+from creds import CLIENT_ID, CLIENT_SECRET
+
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+                    level=logging.DEBUG,
+                    filename='r2m.log')
+
+
+def generate_tag():
+    tag = 'FalconGroupingTags/r2m-'
+    tag += ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
+    return tag
+
+
+def cmd_line():
+    parser = ArgumentParser()
+    parser.add_argument('--debug', action='store_true', required=False)
+    parser.add_argument('--dry-run', action='store_true', required=False)
+    parser.add_argument('-d', '--days', required=False, default='7')
+    parser.add_argument('-f', '--from', required=False, default='', dest='dt_from')  # noqa
+    parser.add_argument('-g', '--group', required=False)
+    parser.add_argument('-t', '--to', required=False, default='', dest='dt_to')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = cmd_line()
+
+    # get today's date
+    today = datetime.datetime.now()
+
+    # build the filter
+    if args.dt_from or args.dt_to:
+        dt_to = None
+        dt_from = None
+        if args.dt_to and not args.dt_from:
+            dt_from = '1970-01-01'
+            dt_to = args.dt_to
+        if args.dt_from:
+            dt_from = args.dt_from
+            if args.dt_to:
+                dt_to = args.dt_to
+            else:
+                dt_to = today.strftime('%Y-%m-%d')
+        f_dev = f"first_seen:>='{dt_from}'+first_seen:<='{dt_to}'"
+        print(f'(>) f_dev: {f_dev}')
+        f_det = f"last_behavior:>='{dt_from}'+last_behavior:<='{dt_to}'"
+        print(f'(>) f_det: {f_det}')
+    else:
+        days = int(args.days)
+        days_ago = (today - datetime.timedelta(days=days)).strftime('%Y-%m-%d')
+        f_dev = f"first_seen:>='{days_ago}'"
+        print(f'(>) f_dev: {f_dev}')
+        f_det = f"last_behavior:>='{days_ago}'"
+        print(f'(>) f_det: {f_det}')
+
+    # init the Falcon api
+    api = APIHarnessV2(client_id=CLIENT_ID,
+                       client_secret=CLIENT_SECRET,
+                       debug=args.debug)
+
+    # get endpoints that were linked in the last N days
+    devices_ids = commons.query_devices_by_filter(api, f_dev, 'first_seen.desc')  # noqa
+    devices = commons.get_device_details(api, devices_ids)
+
+    # get detections that appeared in the last N days
+    detections_ids = commons.query_detects(api, f_det)
+    detections = commons.get_detect_summaries(api, detections_ids)
+
+    # do the dirty job
+    to_be_tagged = []
+    table = []
+    for d in devices:
+        if args.group:
+            if ('groups' not in d) or (args.group not in d['groups']):
+                continue
+        device_id = d.get('device_id')
+        hostname = d.get('hostname')
+        first_seen = d.get('first_seen')[0:10]
+        dts = [dt for dt in detections if dt['device']['device_id'] == device_id]  # noqa
+        n_detections = len(dts)
+        status = 'safe'
+        if n_detections:
+            status = 'unsafe'
+        else:
+            to_be_tagged.append(device_id)
+        table.append([status, n_detections, first_seen, hostname, device_id])
+    headers = ['Status', 'Detections', 'First Seen', 'Hostname', 'Device ID']
+    print(tabulate(table, headers))
+
+    # apply tag
+    tags = [generate_tag()]
+    if not args.dry_run:
+        commons.update_device_tags(api, to_be_tagged, 'add', tags)
+    print(f'(*) You can safely move hosts with tags: {",".join(tags)}')
